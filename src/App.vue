1ede60b6416271339aca43af830daa845ecabf8f
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2020-12-25 15:38:50 -0300

    chore: initialized that for the 4th time

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <template>
-  <router-view />
+  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+    <router-view />
+    <Footer />
+  </main>
 </template>
-
-<style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
